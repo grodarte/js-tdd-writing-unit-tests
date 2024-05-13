8df01c02ea5d5839c315a9c09e4f749de3bd8f9f
Gabrielle <ms.grodarte@gmail.com>
2024-05-13 11:07:28 -0700

    pass test

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+export function pointsForWord(word) {
+    let points = 0
+
+    for(const letter of word){
+        if(["a", "e", "i", "o", "u"].includes(letter)){
+            points += 1
+        } else {
+            points += 2
+        }
+    }
+    return points
+}
